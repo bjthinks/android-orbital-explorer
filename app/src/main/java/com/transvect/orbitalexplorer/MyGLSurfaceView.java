b4a18b6cd711def806433a9c1820dacc1d96a725
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2015-08-26 12:00:00 -0700

    Inflate OpenGLSurfaceView from XML and get rid of a bunch of cruft

--- app/src/main/java/com/transvect/orbitalexplorer/MyGLSurfaceView.java
+++ app/src/main/java/com/transvect/orbitalexplorer/MyGLSurfaceView.java
@@ -2,6 +2,7 @@ package com.transvect.orbitalexplorer;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
 
 /**
  * Created by bwj on 8/25/15.
@@ -12,6 +13,21 @@ public class MyGLSurfaceView extends GLSurfaceView {
 
     public MyGLSurfaceView(Context context) {
         super(context);
+
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2);
+
+        mRenderer = new MyGLRenderer();
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer(mRenderer);
+
+        // Render the view only when there is a change in the drawing data
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+    public MyGLSurfaceView(Context context, AttributeSet attribs) {
+        super(context, attribs);
 
         // Create an OpenGL ES 2.0 context
         setEGLContextClientVersion(2);
