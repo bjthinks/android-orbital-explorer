22e59b2909cdcf7384dbdb490e39a45a8242b4e5
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2015-08-26 23:42:42 -0700

    Run vertex and fragment shaders

--- app/src/main/java/com/transvect/orbitalexplorer/OrbitalRenderer.java
+++ app/src/main/java/com/transvect/orbitalexplorer/OrbitalRenderer.java
@@ -1,21 +1,96 @@
 package com.transvect.orbitalexplorer;
 
 import android.opengl.GLES20;
+import android.util.Log;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 /**
  * The OpenGL guts start here.
  */
 
 public class OrbitalRenderer extends MyGLRenderer {
+
+    private final String TAG = "OrbitalRenderer";
+
+    private FloatBuffer vertexBuffer;
+    private int mProgram;
+
+    private final String vertexShaderSource
+            = "attribute vec2 inPosition;"
+            + "varying vec4 position;"
+            + "void main() {"
+            + "  position = vec4(inPosition.xy, 0, 1);"
+            + "  gl_Position = position;"
+            + "}";
+    private final String fragmentShaderSource
+            = "precision mediump float;"
+            + "varying vec4 position;"
+            + "void main() {"
+            + "  gl_FragColor = vec4((position.xy + 1.0) / 2.0, (2.0 - position.x - position.y) / 4.0, 1);"
+            + "}";
+
+    private int loadShader(int type, String shaderCode) {
+        Log.d(TAG, "loadShader()");
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+        int[] params = {0};
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, params, 0);
+        if (params[0] == 0) {
+            Log.e(TAG, "shader failed to compile");
+            Log.e(TAG, GLES20.glGetShaderInfoLog(shader));
+        }
+        return shader;
+    }
+
+    OrbitalRenderer() {
+        float squareCoordinates[] = {
+                -1.0f, -1.0f,
+                -1.0f,  1.0f,
+                 1.0f,  1.0f,
+                 1.0f, -1.0f,
+        };
+        ByteBuffer bb = ByteBuffer.allocateDirect(squareCoordinates.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(squareCoordinates);
+        vertexBuffer.position(0);
+    }
+
     @Override
     public void onCreate(int width, int height, boolean contextIsNew) {
         if (contextIsNew) {
-            GLES20.glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
+            Log.d(TAG, "onCreate() -> context is new");
+            int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderSource);
+            int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderSource);
+            mProgram = GLES20.glCreateProgram();
+            GLES20.glAttachShader(mProgram, vertexShader);
+            GLES20.glAttachShader(mProgram, fragmentShader);
+            GLES20.glLinkProgram(mProgram);
+            GLES20.glClearColor(1.0f, 0.75f, 0.0f, 1.0f);
+            getGLError();
         }
     }
 
     @Override
     public void onDrawFrame() {
+        Log.d(TAG, "onDrawFrame()");
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glUseProgram(mProgram);
+        int inPositionHandle = GLES20.glGetAttribLocation(mProgram, "inPosition");
+        GLES20.glEnableVertexAttribArray(inPositionHandle);
+        GLES20.glVertexAttribPointer(inPositionHandle, 2, GLES20.GL_FLOAT, false, 8, vertexBuffer);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 4);
+        GLES20.glDisableVertexAttribArray(inPositionHandle);
+        getGLError();
+    }
+
+    private void getGLError() {
+        int error = GLES20.glGetError();
+        if (error > 0)
+            Log.e(TAG, "OpenGL error code " + error);
     }
 }
