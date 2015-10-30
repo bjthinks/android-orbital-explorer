f071c4631dcddc2d1bee826f61cbb7b05b6a4b51
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2015-10-30 11:17:40 -0700

    Finish Polynomial (add, subtract, multiply)

--- app/src/main/java/com/transvect/orbitalexplorer/MainActivity.java
+++ app/src/main/java/com/transvect/orbitalexplorer/MainActivity.java
@@ -43,8 +43,6 @@ public class MainActivity extends Activity {
 
         // Render the view only when there is a change in the drawing data
         mOrbitalView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-
-        Polynomial.test();
     }
 
     private boolean hasGLES30() {
--- app/src/main/java/com/transvect/orbitalexplorer/Polynomial.java
+++ app/src/main/java/com/transvect/orbitalexplorer/Polynomial.java
@@ -3,16 +3,17 @@ package com.transvect.orbitalexplorer;
 import android.util.Log;
 
 public class Polynomial {
+    private static final String TAG = "Polynomial";
 
     // Invariant: c is not null, and leading coeff is not 0.
     // The zero polynomial has c.length == 0 and degree -1.
     private double c[];
 
-    Polynomial() {
+    public Polynomial() {
         c = new double[0];
     }
 
-    Polynomial(double cc) {
+    public Polynomial(double cc) {
         if (cc == 0.)
             c = new double[0];
         else {
@@ -21,11 +22,11 @@ public class Polynomial {
         }
     }
 
-    int degree() {
+    public int degree() {
         return c.length - 1;
     }
 
-    static Polynomial variable() {
+    public static Polynomial variable() {
         Polynomial x = new Polynomial();
         x.c = new double[2];
         x.c[0] = 0.;
@@ -42,16 +43,31 @@ public class Polynomial {
         return result;
     }
 
-    /* Polynomial add(Polynomial rhs) {
-    } */
+    public Polynomial add(Polynomial rhs) {
+        Polynomial result = new Polynomial();
 
-    /* Polynomial subtract(Polynomial rhs) {
-    } */
+        int needToDoAdditionUpTo;
+        if (degree() < rhs.degree()) {
+            result.c = new double[rhs.c.length];
+            System.arraycopy(rhs.c, c.length, result.c, c.length, rhs.c.length - c.length);
+            needToDoAdditionUpTo = c.length;
+        } else if (degree() > rhs.degree()) {
+            result.c = new double[c.length];
+            System.arraycopy(c, rhs.c.length, result.c, rhs.c.length, c.length - rhs.c.length);
+            needToDoAdditionUpTo = rhs.c.length;
+        } else {
+            int len = c.length;
+            while (len > 0 && c[len - 1] + rhs.c[len - 1] == 0)
+                --len;
+            result.c = new double[len];
+            needToDoAdditionUpTo = len;
+        }
+        for (int i = 0; i < needToDoAdditionUpTo; ++i)
+            result.c[i] = c[i] + rhs.c[i];
+        return result;
+    }
 
-    /* Polynomial multiply(double c) {
-    } */
-
-    Polynomial negate() {
+    public Polynomial negate() {
         Polynomial result = new Polynomial();
         result.c = new double[c.length];
         for (int i = 0; i < c.length; ++i) {
@@ -60,7 +76,36 @@ public class Polynomial {
         return result;
     }
 
-    static void test() {
+    public Polynomial subtract(Polynomial rhs) {
+        return add(rhs.negate());
+    }
+
+    public Polynomial multiply(Polynomial rhs) {
+        Polynomial result = new Polynomial();
+        if (c.length == 0 || rhs.c.length == 0)
+            return result;
+        result.c = new double[c.length + rhs.c.length - 1];
+        for (int i = 0; i < c.length; ++i)
+            for (int j = 0; j < rhs.c.length; ++j)
+                result.c[i + j] += c[i] * rhs.c[j];
+        if (result.c[result.c.length - 1] == 0.)
+            Log.w(TAG, "Polynomial multiplication underflow");
+        return result;
+    }
+
+    public Polynomial add(double c) {
+        return add(new Polynomial(c));
+    }
+
+    public Polynomial subtract(double c) {
+        return subtract(new Polynomial(c));
+    }
+
+    public Polynomial multiply(double c) {
+        return multiply(new Polynomial(c));
+    }
+
+    /* public static void test() {
         String TAG = "Polynomial";
         Log.d(TAG, "Testing");
         TAG += " FAIL";
@@ -91,19 +136,73 @@ public class Polynomial {
         if (x.eval(1) != 1.)
             Log.w(TAG, "x.e(1)");
 
-        if (zero.negate().eval(0) != 0.)
-            Log.w(TAG, "0.n().e(0)");
-        if (zero.negate().eval(1) != 0.)
-            Log.w(TAG, "0.n().e(1)");
-        if (one.negate().eval(0) != -1.)
-            Log.w(TAG, "1.n().e(0)");
-        if (one.negate().eval(1) != -1.)
-            Log.w(TAG, "1.n().e(1)");
+        testSame("-0", zero.negate(), zero);
+        testSame("-1", one.negate(), new Polynomial(-1));
         if (x.negate().eval(0) != 0.)
             Log.w(TAG, "x.n().e(0)");
         if (x.negate().eval(1) != -1.)
             Log.w(TAG, "x.n().e(1)");
 
+        testSame("0+0", zero.add(zero), zero);
+        testSame("0+1", zero.add(one), one);
+        testSame("1+0", one.add(zero), one);
+        testSame("1+1", one.add(one), new Polynomial(2));
+        testSame("0+x", zero.add(x), x);
+        testSame("x+0", x.add(zero), x);
+        Polynomial xplusone = Polynomial.variable();
+        xplusone.c[0] = 1;
+        testSame("1+x", one.add(x), xplusone);
+        testSame("x+1", x.add(one), xplusone);
+        Polynomial twox = Polynomial.variable();
+        twox.c[1] = 2;
+        testSame("x+x", x.add(x), twox);
+
+        Polynomial negone = one.negate();
+        testSame("1+-1", one.add(negone), zero);
+        Polynomial negx = x.negate();
+        testSame("x+-x", x.add(negx), zero);
+        testSame("(1+x)+-x=1", one.add(x).add(negx), one);
+        testSame("(1+x)+-1=x", one.add(x).add(negone), x);
+
+        testSame("1-x", one.subtract(x), one.add(negx));
+        testSame("x-1", x.subtract(one), x.add(negone));
+
+        testSame("0*0", zero.multiply(zero), zero);
+        testSame("0*1", zero.multiply(one), zero);
+        testSame("1*0", one.multiply(zero), zero);
+        testSame("1*1", one.multiply(one), one);
+        testSame("x*0", x.multiply(zero), zero);
+        testSame("x*1", x.multiply(one), x);
+        testSame("0*x", zero.multiply(x), zero);
+        testSame("1*x", one.multiply(x), x);
+        Polynomial x2 = new Polynomial();
+        x2.c = new double[3];
+        x2.c[0] = 0;
+        x2.c[1] = 0;
+        x2.c[2] = 1;
+        testSame("x*x", x.multiply(x), x2);
+
+        // (x-2) * (x+3) = x^2 + x - 6
+        testSame("(x-2)(x+3)", x.subtract(new Polynomial(2)).multiply(x.add(new Polynomial(3))),
+                x2.add(x).subtract(new Polynomial(6)));
+        testSame("(x-2)(x+3)", x.subtract(2).multiply(x.add(3)),
+                x2.add(x).subtract(6));
+
+        // x+x = 2x
+        testSame("x+x", x.add(x), x.multiply(2));
+
         Log.d("Polynomial", "Test done");
     }
+
+    private static void testSame(String tag, Polynomial f, Polynomial g) {
+        tag = "testSame " + tag;
+        if (f.degree() != g.degree())
+            Log.w(tag, "degree");
+        if (f.eval(0.) != g.eval(0.))
+            Log.w(tag, "eval(0)");
+        if (f.eval(1.) != g.eval(1.))
+            Log.w(tag, "eval(1)");
+        if (f.eval(-5.) != g.eval(-5.))
+            Log.w(tag, "eval(-5)");
+    } */
 }
