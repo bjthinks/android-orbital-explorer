a253377cecbca41cf8ff731ffa019afbd161c15f
Brian Johnson (NUC) <bjthinks@gmail.com>
2015-11-20 23:26:41 -0800

    Simplify OrthogonalPolynomials

--- app/src/main/java/com/transvect/orbitalexplorer/OrthogonalPolynomials.java
+++ app/src/main/java/com/transvect/orbitalexplorer/OrthogonalPolynomials.java
@@ -16,10 +16,8 @@ public class OrthogonalPolynomials {
                 new Product(Polynomial.variable(), w)) / MyMath.rombergIntegrate(w));
         for (int i = 2; i < n; ++i) {
             double B = innerProduct(new Product(Polynomial.variable(), foofoo[i - 1]),
-                    foofoo[i - 1])
-                    / innerProduct(foofoo[i - 1], foofoo[i - 1]);
-            double A = innerProduct(foofoo[i - 1], foofoo[i - 1])
-                    / innerProduct(foofoo[i - 2], foofoo[i - 2]);
+                    foofoo[i - 1]) / normSquared(foofoo[i - 1]);
+            double A = normSquared(foofoo[i - 1]) / normSquared(foofoo[i - 2]);
             foofoo[i] = foofoo[i - 1].multiply(Polynomial.variable().subtract(B))
                     .subtract(foofoo[i - 2].multiply(A));
             Log.d(TAG, i + ":       " + foofoo[i].toString());
@@ -29,6 +27,12 @@ public class OrthogonalPolynomials {
                 rootsString += " " + roots[j];
             Log.d(TAG, i + " roots:" + rootsString);
         }
+    }
+
+    private double normSquared(Function f) {
+        Function f2 = new Square(f);
+        Function integrand = new Product(f2, w2);
+        return MyMath.rombergIntegrate(integrand);
     }
 
     private double innerProduct(Function f, Function g) {
