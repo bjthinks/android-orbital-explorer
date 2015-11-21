39e380a68db673e06d3780a9d31f92fbf03e710a
Brian Johnson (NUC) <bjthinks@gmail.com>
2015-11-20 22:54:24 -0800

    Improve OrthogonalPolynomials to use the "three term recurrence relations"

--- app/src/main/java/com/transvect/orbitalexplorer/OrthogonalPolynomials.java
+++ app/src/main/java/com/transvect/orbitalexplorer/OrthogonalPolynomials.java
@@ -5,25 +5,26 @@ import android.util.Log;
 public class OrthogonalPolynomials {
     private static final String TAG = "OrthogonalPolynomials";
 
-    public OrthogonalPolynomials() {
-        int n = 5;
+    public OrthogonalPolynomials(Function w) {
+        int n = 8;
         Polynomial[] foofoo = new Polynomial[n];
-        Barbar barbar = new Barbar();
-        for (int i = 0; i < n; ++i) {
-            foofoo[i] = Polynomial.variableToThe(i);
-            for (int j = 0; j < i; ++j)
-                foofoo[i] = foofoo[i].subtract(foofoo[j].multiply(
-                        MyMath.rombergIntegrate(new Product(barbar,
-                                new Product(foofoo[i], foofoo[j])))));
-            foofoo[i] = foofoo[i].multiply(1.0 / Math.sqrt(
-                    MyMath.rombergIntegrate(new Product(barbar,
-                            new Product(foofoo[i], foofoo[i])))));
-            Log.d(TAG, i + ":   " + foofoo[i].toString());
-        }
-    }
-    private class Barbar implements Function {
-        public double eval(double x) {
-            return Math.exp(-Math.abs(x));
+        foofoo[0] = new Polynomial(1.0);
+        foofoo[1] = Polynomial.variable().subtract(MyMath.rombergIntegrate(
+                new Product(Polynomial.variable(), w)) / MyMath.rombergIntegrate(w));
+        for (int i = 2; i < n; ++i) {
+            double B = MyMath.rombergIntegrate(new Product(new Product(Polynomial.variable(),
+                    new Product(foofoo[i - 1], foofoo[i - 1])), w))
+                    / MyMath.rombergIntegrate(new Product(new Product(foofoo[i - 1], foofoo[i - 1]), w));
+            double A = MyMath.rombergIntegrate(new Product(new Product(foofoo[i - 1], foofoo[i - 1]), w))
+                    / MyMath.rombergIntegrate(new Product(new Product(foofoo[i - 2], foofoo[i - 2]), w));
+            foofoo[i] = foofoo[i - 1].multiply(Polynomial.variable().subtract(B))
+                    .subtract(foofoo[i - 2].multiply(A));
+            Log.d(TAG, i + ":       " + foofoo[i].toString());
+            double[] roots = MyMath.solvePolynomial(foofoo[i]);
+            String rootsString = "";
+            for (int j = 0; j < roots.length; ++j)
+                rootsString += " " + roots[j];
+            Log.d(TAG, i + " roots:" + rootsString);
         }
     }
 }
