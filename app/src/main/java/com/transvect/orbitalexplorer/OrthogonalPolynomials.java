957570e74f11943e3de1921afb8e7a5829a10bf6
Brian Johnson (NUC) <bjthinks@gmail.com>
2015-11-20 23:21:38 -0800

    Simplify OrthogonalPolynomials

--- app/src/main/java/com/transvect/orbitalexplorer/OrthogonalPolynomials.java
+++ app/src/main/java/com/transvect/orbitalexplorer/OrthogonalPolynomials.java
@@ -5,18 +5,21 @@ import android.util.Log;
 public class OrthogonalPolynomials {
     private static final String TAG = "OrthogonalPolynomials";
 
+    Function w2;
+
     public OrthogonalPolynomials(Function w) {
+        w2 = w; //new Product(w, w);
         int n = 8;
         Polynomial[] foofoo = new Polynomial[n];
         foofoo[0] = new Polynomial(1.0);
         foofoo[1] = Polynomial.variable().subtract(MyMath.rombergIntegrate(
                 new Product(Polynomial.variable(), w)) / MyMath.rombergIntegrate(w));
         for (int i = 2; i < n; ++i) {
-            double B = MyMath.rombergIntegrate(new Product(new Product(Polynomial.variable(),
-                    new Product(foofoo[i - 1], foofoo[i - 1])), w))
-                    / MyMath.rombergIntegrate(new Product(new Product(foofoo[i - 1], foofoo[i - 1]), w));
-            double A = MyMath.rombergIntegrate(new Product(new Product(foofoo[i - 1], foofoo[i - 1]), w))
-                    / MyMath.rombergIntegrate(new Product(new Product(foofoo[i - 2], foofoo[i - 2]), w));
+            double B = innerProduct(new Product(Polynomial.variable(), foofoo[i - 1]),
+                    foofoo[i - 1])
+                    / innerProduct(foofoo[i - 1], foofoo[i - 1]);
+            double A = innerProduct(foofoo[i - 1], foofoo[i - 1])
+                    / innerProduct(foofoo[i - 2], foofoo[i - 2]);
             foofoo[i] = foofoo[i - 1].multiply(Polynomial.variable().subtract(B))
                     .subtract(foofoo[i - 2].multiply(A));
             Log.d(TAG, i + ":       " + foofoo[i].toString());
@@ -26,5 +29,11 @@ public class OrthogonalPolynomials {
                 rootsString += " " + roots[j];
             Log.d(TAG, i + " roots:" + rootsString);
         }
+    }
+
+    private double innerProduct(Function f, Function g) {
+        Function fg = new Product(f, g);
+        Function integrand = new Product(fg, w2);
+        return MyMath.rombergIntegrate(integrand);
     }
 }
