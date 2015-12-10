b2b9d310c74553f52689f1a3e9c4445078b0894f
Brian Johnson (NUC) <bjthinks@gmail.com>
2015-12-10 14:38:27 -0800

    Add commented-out code to change settings summary to match current setting

--- app/src/main/java/com/transvect/orbitalexplorer/SettingsFragment.java
+++ app/src/main/java/com/transvect/orbitalexplorer/SettingsFragment.java
@@ -1,12 +1,46 @@
 package com.transvect.orbitalexplorer;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.util.Log;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment
+        /* implements SharedPreferences.OnSharedPreferenceChangeListener */ {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
     }
+
+    /* @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+        ListPreference listPref = (ListPreference)
+                findPreference("prefColor");
+        if (listPref != null)
+            listPref.setSummary(listPref.getEntry());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceManager().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference pref = findPreference(key);
+
+        if (pref instanceof ListPreference) {
+            ListPreference listPref = (ListPreference) pref;
+            pref.setSummary(listPref.getEntry());
+        }
+    } */
 }
