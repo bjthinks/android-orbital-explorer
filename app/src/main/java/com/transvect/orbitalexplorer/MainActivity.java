1aead736b505d447c743b85a3dd7f1597ec2dabb
Brian Johnson (NUC) <bjthinks@gmail.com>
2015-12-14 04:37:32 -0800

    Use Toolbar instead of default action bar

--- app/src/main/java/com/transvect/orbitalexplorer/MainActivity.java
+++ app/src/main/java/com/transvect/orbitalexplorer/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import java.util.Random;
 
@@ -30,11 +31,28 @@ public class MainActivity extends Activity {
         // Inflate a GLSurfaceView instance and set it
         // as the ContentView for this Activity.
         setContentView(R.layout.activity_main);
+
+        // Inflate a Toolbar instance and set it
+        // as the ActionBar for this Activity
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+
+        // Adjust toolbar downward so it doesn't overlap the Status Bar
+        toolbar.setPadding(0, getStatusBarHeight(), 0, 0);
+
         mOrbitalView = (OrbitalView) findViewById(R.id.orbitalview);
         // Make an OrbitalRenderer. Needs assets for shader code.
         OrbitalRenderer renderer = new OrbitalRenderer(mOrbitalView, this);
         // Start the rendering thread
         mOrbitalView.setRenderer(renderer);
+    }
+
+    private int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0)
+            result = getResources().getDimensionPixelSize(resourceId);
+        return result;
     }
 
     private boolean hasGLES30() {
