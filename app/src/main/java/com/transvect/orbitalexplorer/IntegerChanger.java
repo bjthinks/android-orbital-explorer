c3b38e3c59946d98162ba9dac8e55a20c39b0a61
Brian Johnson (NUC) <bjthinks@gmail.com>
2016-01-15 01:40:28 -0800

    Working on IntegerChanger

---
+++ app/src/main/java/com/transvect/orbitalexplorer/IntegerChanger.java
@@ -0,0 +1,75 @@
+package com.transvect.orbitalexplorer;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class IntegerChanger extends LinearLayout {
+
+    private static final String TAG = "IntegerChanger";
+
+    private ImageButton upArrow;
+    private ImageButton downArrow;
+    private TextView text;
+    private Integer value;
+
+    public IntegerChanger(Context context) {
+        super(context);
+        constructorSetup(context);
+    }
+
+    public IntegerChanger(Context context, AttributeSet attribs) {
+        super(context, attribs);
+        constructorSetup(context);
+    }
+
+    public IntegerChanger(Context context, AttributeSet attribs, int defStyle) {
+        super(context, attribs, defStyle);
+        constructorSetup(context);
+    }
+
+    private void constructorSetup(Context context) {
+        setOrientation(VERTICAL);
+        LayoutInflater inflater
+                = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.integerchanger_view, this);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        upArrow = (ImageButton) findViewById(R.id.integerchanger_uparrow);
+        downArrow = (ImageButton) findViewById(R.id.integerchanger_downarrow);
+        text = (TextView) findViewById(R.id.integerchanger_value);
+
+        upArrow.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setValue(value + 1);
+            }
+        });
+        downArrow.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setValue(value - 1);
+            }
+        });
+
+        setValue(0);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int v) {
+        value = v;
+        text.setText(value.toString());
+    }
+}
