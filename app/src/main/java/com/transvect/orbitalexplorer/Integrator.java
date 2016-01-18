2b8f669a182e207e66812fd39052bec79e8f8c43
Brian Johnson (NUC) <bjthinks@gmail.com>
2016-01-18 15:27:13 -0800

    Continue refactoring

--- app/src/main/java/com/transvect/orbitalexplorer/Integrator.java
+++ app/src/main/java/com/transvect/orbitalexplorer/Integrator.java
@@ -3,39 +3,41 @@ package com.transvect.orbitalexplorer;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.opengl.GLES30;
-import android.util.Log;
+
 import java.nio.FloatBuffer;
 
 public class Integrator extends RenderStage {
     private static final String TAG = "Integrator";
 
-    RenderPreferences mRenderPreferences;
+    RenderPreferences renderPreferences;
 
-    private int mProgram;
-    private FloatBuffer mVertexBuffer;
+    private int program;
+    private FloatBuffer vertexBuffer;
 
     Orbital orbital;
 
     private int radialDataSize;
-    private Texture mRadialTexture;
+    private Texture radialTexture;
 
     private int azimuthalDataSize;
-    private Texture mAzimuthalTexture;
+    private Texture azimuthalTexture;
 
-    private Texture mQuadratureWeightTexture;
-    private Texture mTexture;
-    private Framebuffer mFramebuffer;
-    private int mWidth, mHeight;
+    private int quadratureDataSize;
+    private Texture quadratureTexture;
+
+    private Texture outputTexture;
+    private Framebuffer framebuffer;
+    private int width, height;
 
     private final double MAXIMUM_RADIUS = 16.0;
     private final int QUADRATURE_SIZE = 64;
 
     public Texture getTexture() {
-        return mTexture;
+        return outputTexture;
     }
 
     Integrator(Context context) {
-        mRenderPreferences = new RenderPreferences(context);
+        renderPreferences = new RenderPreferences(context);
 
         float squareCoordinates[] = {
                 -1.0f, -1.0f,
@@ -43,7 +45,7 @@ public class Integrator extends RenderStage {
                 1.0f,  1.0f,
                 1.0f, -1.0f,
         };
-        mVertexBuffer = floatArrayToBuffer(squareCoordinates);
+        vertexBuffer = floatArrayToBuffer(squareCoordinates);
 
         orbital = new Orbital(8, 6, 4, 1);
     }
@@ -51,30 +53,26 @@ public class Integrator extends RenderStage {
     public void newContext(AssetManager assetManager) {
 
         // Create radial texture
-        mRadialTexture = new Texture(GLES30.GL_RG, GLES30.GL_FLOAT, GLES30.GL_RG32F);
+        radialTexture = new Texture(GLES30.GL_RG, GLES30.GL_FLOAT, GLES30.GL_RG32F);
         float[] radialData = orbital.getRadialData();
         radialDataSize = radialData.length / 2;
-        mRadialTexture.bindToTexture2DAndSetImage(radialDataSize, 1, radialData);
+        radialTexture.bindToTexture2DAndSetImage(radialDataSize, 1, radialData);
 
         // Floating point textures are not filterable
         setTexture2DMinMagFilters(GLES30.GL_NEAREST, GLES30.GL_NEAREST);
 
         // Create azimuthal texture
-        mAzimuthalTexture = new Texture(GLES30.GL_RG, GLES30.GL_FLOAT, GLES30.GL_RG32F);
+        azimuthalTexture = new Texture(GLES30.GL_RG, GLES30.GL_FLOAT, GLES30.GL_RG32F);
         float[] azimuthalData = orbital.getAzimuthalData();
         azimuthalDataSize = azimuthalData.length / 2;
-        mAzimuthalTexture.bindToTexture2DAndSetImage(azimuthalDataSize, 1, azimuthalData);
+        azimuthalTexture.bindToTexture2DAndSetImage(azimuthalDataSize, 1, azimuthalData);
 
         // Floating point textures are not filterable
         setTexture2DMinMagFilters(GLES30.GL_NEAREST, GLES30.GL_NEAREST);
 
-        final int quadratureFormat = GLES30.GL_RGBA;
-        final int quadratureType = GLES30.GL_FLOAT;
-        final int quadratureInternalFormat = GLES30.GL_RGBA32F;
-        // Create quadrature weight texture
-        mQuadratureWeightTexture
-                = new Texture(quadratureFormat, quadratureType, quadratureInternalFormat);
-        mQuadratureWeightTexture.bindToTexture2DAndSetImage(
+        // Create quadrature texture
+        quadratureTexture = new Texture(GLES30.GL_RGBA, GLES30.GL_FLOAT, GLES30.GL_RGBA32F);
+        quadratureTexture.bindToTexture2DAndSetImage(
                 orbital.getQuadraturePoints(), QUADRATURE_SIZE, orbital.getQuadratureData());
 
         // Floating point textures are not filterable
@@ -85,8 +83,8 @@ public class Integrator extends RenderStage {
         // OpenGL ES 3.0 specification, or we will get an OpenGL error. We also
         // need to choose a sized internal format which is color-renderable
         // according to Table 3.13 (in the absence of extensions).
-        mTexture = new Texture(GLES30.GL_RGBA_INTEGER, GLES30.GL_SHORT, GLES30.GL_RGBA16I);
-        mTexture.bindToTexture2DAndResize(1, 1);
+        outputTexture = new Texture(GLES30.GL_RGBA_INTEGER, GLES30.GL_SHORT, GLES30.GL_RGBA16I);
+        outputTexture.bindToTexture2DAndResize(1, 1);
 
         // Set the filters for sampling the bound texture, when sampling at
         // a different resolution than native.
@@ -96,46 +94,46 @@ public class Integrator extends RenderStage {
         GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D,
                 GLES30.GL_TEXTURE_WRAP_T, GLES30.GL_CLAMP_TO_EDGE);
 
-        mFramebuffer = new Framebuffer();
-        mFramebuffer.bindAndSetTexture(mTexture);
+        framebuffer = new Framebuffer();
+        framebuffer.bindAndSetTexture(outputTexture);
 
         getGLError();
 
         // Compile & link GLSL program
         Shader vertexShader = new Shader(assetManager, "integrator.vert", GLES30.GL_VERTEX_SHADER);
         Shader fragmentShader = new Shader(assetManager, "integrator.frag", GLES30.GL_FRAGMENT_SHADER);
-        mProgram = GLES30.glCreateProgram();
-        GLES30.glAttachShader(mProgram, vertexShader.getId());
-        GLES30.glAttachShader(mProgram, fragmentShader.getId());
-        GLES30.glLinkProgram(mProgram);
+        program = GLES30.glCreateProgram();
+        GLES30.glAttachShader(program, vertexShader.getId());
+        GLES30.glAttachShader(program, fragmentShader.getId());
+        GLES30.glLinkProgram(program);
         getGLError();
     }
 
-    public void resize(int width, int height) {
-        mWidth = width;
-        mHeight = height;
-        mTexture.bindToTexture2DAndResize(mWidth, mHeight);
+    public void resize(int w, int h) {
+        width = w;
+        height = h;
+        outputTexture.bindToTexture2DAndResize(width, height);
     }
 
     public void render(float[] shaderTransform) {
 
-        mFramebuffer.bindToAttachmentPoint();
-        GLES30.glViewport(0, 0, mWidth, mHeight);
-        GLES30.glUseProgram(mProgram);
+        framebuffer.bindToAttachmentPoint();
+        GLES30.glViewport(0, 0, width, height);
+        GLES30.glUseProgram(program);
 
         GLES30.glActiveTexture(GLES30.GL_TEXTURE0);
-        mRadialTexture.bindToTexture2D();
+        radialTexture.bindToTexture2D();
         setUniformInt("radial", 0);
 
         GLES30.glActiveTexture(GLES30.GL_TEXTURE1);
-        mAzimuthalTexture.bindToTexture2D();
+        azimuthalTexture.bindToTexture2D();
         setUniformInt("azimuthal", 1);
 
         GLES30.glActiveTexture(GLES30.GL_TEXTURE2);
-        mQuadratureWeightTexture.bindToTexture2D();
+        quadratureTexture.bindToTexture2D();
         setUniformInt("quadrature", 2);
 
-        setUniformInt("colorMode", mRenderPreferences.getColorMode());
+        setUniformInt("colorMode", renderPreferences.getColorMode());
         setUniformInt("numQuadraturePoints", orbital.getQuadraturePoints());
 
         setUniformFloat("exponentialConstant", (float) (2.0 * orbital.getRadialExponent()));
@@ -151,12 +149,12 @@ public class Integrator extends RenderStage {
         setUniformFloat("zero", 0.0f);
         setUniformFloat("one", 1.0f);
 
-        int mvpMatrixHandle = GLES30.glGetUniformLocation(mProgram, "shaderTransform");
+        int mvpMatrixHandle = GLES30.glGetUniformLocation(program, "shaderTransform");
         GLES30.glUniformMatrix4fv(mvpMatrixHandle, 1, false, shaderTransform, 0);
 
-        int inPositionHandle = GLES30.glGetAttribLocation(mProgram, "inPosition");
+        int inPositionHandle = GLES30.glGetAttribLocation(program, "inPosition");
         GLES30.glEnableVertexAttribArray(inPositionHandle);
-        GLES30.glVertexAttribPointer(inPositionHandle, 2, GLES30.GL_FLOAT, false, 8, mVertexBuffer);
+        GLES30.glVertexAttribPointer(inPositionHandle, 2, GLES30.GL_FLOAT, false, 8, vertexBuffer);
 
         final int zeroes[] = {0, 0, 0, 0};
         GLES30.glClearBufferiv(GLES30.GL_COLOR, 0, zeroes, 0);
@@ -166,12 +164,12 @@ public class Integrator extends RenderStage {
     }
 
     void setUniformInt(String name, int value) {
-        int handle = GLES30.glGetUniformLocation(mProgram, name);
+        int handle = GLES30.glGetUniformLocation(program, name);
         GLES30.glUniform1i(handle, value);
     }
 
     void setUniformFloat(String name, float value) {
-        int handle = GLES30.glGetUniformLocation(mProgram, name);
+        int handle = GLES30.glGetUniformLocation(program, name);
         GLES30.glUniform1f(handle, value);
     }
 }
