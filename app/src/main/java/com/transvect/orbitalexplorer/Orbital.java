c5440c430584d15de7161fcf36cf19cd8fac20b0
Brian Johnson (NUC) <bjthinks@gmail.com>
2016-01-26 13:17:40 -0800

    Promote WeightFunction from inner to public class

--- app/src/main/java/com/transvect/orbitalexplorer/Orbital.java
+++ app/src/main/java/com/transvect/orbitalexplorer/Orbital.java
@@ -91,34 +91,4 @@ public class Orbital {
         }
         return quadratureWeights;
     }
-
-    private static class WeightFunction implements Function {
-        private double exponentialConstant;
-        private int powerOfR;
-        private double mDistanceFromOrigin;
-
-        public WeightFunction(double exponentialConstant_,
-                              int powerOfR_,
-                              double distanceFromOrigin) {
-            exponentialConstant = exponentialConstant_;
-            powerOfR = powerOfR_;
-            mDistanceFromOrigin = distanceFromOrigin;
-        }
-
-        public double eval(double x) {
-            if (x < 0.0)
-                return 0.0;
-
-            double r = Math.sqrt(mDistanceFromOrigin * mDistanceFromOrigin + x * x);
-            // Multiply by 2 because the wave function is squared
-            double value = Math.exp(2.0 * exponentialConstant * r);
-            // Multiply by 2 because the wave function is squared
-            value *= Math.pow(r, 2.0 * powerOfR);
-
-            if (x == 0.0)
-                value *= 0.5;
-
-            return value;
-        }
-    }
 }
