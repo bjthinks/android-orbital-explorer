fc129f541cfc3b698e07aecf7c65ff01cf137265
Brian Johnson (NUC) <bjthinks@gmail.com>
2016-02-08 11:49:30 -0800

    Add a module for code generation

---
+++ quadrature/src/main/java/com/example/QuadratureGenerator.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class QuadratureGenerator {
+
+    public static void main(String args[]) {
+        System.out.print("This is a standalone java program\n");
+    }
+
+}
