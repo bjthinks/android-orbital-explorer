a961e7287d7d755bf77beed2adf7c189ad94d4d9
Brian Johnson (NUC) <bjthinks@gmail.com>
2016-02-09 18:33:56 -0800

    Start moving common code into math library

--- quadrature/src/main/java/com/example/CodePrinter.java
+++ quadrature/src/main/java/com/example/CodePrinter.java
@@ -8,19 +8,66 @@ public class CodePrinter {
     private static final String filename =
             "app/src/main/java/com/transvect/orbitalexplorer/QuadratureTables.java";
     private PrintWriter writer;
+    private int N = 0;
+    private int L = -1;
 
     private void line(String s) {
         writer.println(s);
+    }
+
+    private void line(int spaces, String s) {
+        for (int i = 0; i < spaces; ++i)
+            writer.print(' ');
+        line(s);
+    }
+
+    private String myToStr(double d) {
+        String r = Double.toString(d);
+        while (r.length() < 19)
+            r += " ";
+        return r;
     }
 
     public void printPreface() throws IOException {
         writer = new PrintWriter(filename, "UTF-8");
         line("package com.transvect.orbitalexplorer;");
         line("");
-        line("public class QuadratureTables {");
+        line("public final class QuadratureTables {");
+        line("");
+        line(4, "private QuadratureTables() {}");
+        line("");
+        line(4, "public final double[][][] quadratureTables = {");
+    }
+
+    public void print(int newN, int newL, double data[]) {
+        if (newN != N) {
+            if (N != 0) {
+                // Close previous
+                line(12, "},");
+            }
+            // Open new
+            line(12, "{");
+            if (newN != N + 1)
+                System.err.println("Severe error: N out of order");
+            N = newN;
+            L = -1;
+        }
+
+        if (newL != L + 1)
+            System.err.println("Severe error: L out of order");
+        L = newL;
+        line(20, "{");
+        line(28, "// N = " + N + ", L = " + L);
+        for (int i = 0; i < data.length; i += 2) {
+            line(28, myToStr(data[i]) + ", " + myToStr(data[i + 1]) + ",");
+        }
+        line(20, "},");
     }
 
     public void printSuffix() {
+        // close last
+        line(12, "}");
+        line(4, "};");
         line("}");
         writer.close();
     }
--- quadrature/src/main/java/com/example/QuadratureGenerator.java
+++ quadrature/src/main/java/com/example/QuadratureGenerator.java
@@ -9,6 +9,14 @@ public class QuadratureGenerator {
     private void go() throws IOException {
         codePrinter = new CodePrinter();
         codePrinter.printPreface();
+        double data[] =
+                {
+                        0.5857864376269006, 0.8535533905932715,
+                        3.414213562373088, 0.14644660940672669
+                };
+        codePrinter.print(1, 0, data);
+        codePrinter.print(2, 0, data);
+        codePrinter.print(2, 1, data);
         codePrinter.printSuffix();
     }
 
