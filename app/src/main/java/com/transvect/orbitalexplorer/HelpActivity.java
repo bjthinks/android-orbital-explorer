10a017242dd1b05a89ff5d62aadcdcb1d0e50dff
Brian Johnson (NUC) <bjthinks@gmail.com>
2016-02-29 13:16:48 -0800

    Remove unused save/restore state functions from HelpActivity

--- app/src/main/java/com/transvect/orbitalexplorer/HelpActivity.java
+++ app/src/main/java/com/transvect/orbitalexplorer/HelpActivity.java
@@ -23,16 +23,4 @@ public class HelpActivity extends AppCompatActivity {
         WebView webview = (WebView) findViewById(R.id.helpview);
         webview.loadUrl(extras.getString("url"));
     }
-
-    // This might happen before or after onPause(), but if it needs to be called,
-    // it will always be called before onStop().
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle inState) {
-        super.onRestoreInstanceState(inState);
-    }
 }
