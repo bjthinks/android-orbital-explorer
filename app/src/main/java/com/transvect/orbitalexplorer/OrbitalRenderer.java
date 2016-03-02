6db13842ae560e4b2f751d4b39af162432875372
Brian Johnson (NUC) <bjthinks@gmail.com>
2016-03-01 18:47:22 -0800

    Remove (mostly untested) performance scaling code & refactor OrbitalRenderer

--- app/src/main/java/com/transvect/orbitalexplorer/OrbitalRenderer.java
+++ app/src/main/java/com/transvect/orbitalexplorer/OrbitalRenderer.java
@@ -33,67 +33,25 @@ public class OrbitalRenderer implements GLSurfaceView.Renderer {
         screenDrawer.newContext();
     }
 
-    private int mWidth = 1;
-    private int mHeight = 1;
-    private float mAspectRatio = 1.0f;
-    private double performanceScalingFactor = 1.0;
+    private float aspectRatio = 1.0f;
 
     // Rendering thread
     @Override
     public void onSurfaceChanged(GL10 unused, int width, int height) {
-        mWidth = width;
-        mHeight = height;
-        mAspectRatio = (float) width / (float) height;
-        resizeIntegration();
-    }
-
-    private void resizeIntegration() {
-        double scaleDownFactor = performanceScalingFactor * 160.0 / Math.max(160.0, dpi);
-        int integrationWidth  = (int) (scaleDownFactor * mWidth);
-        int integrationHeight = (int) (scaleDownFactor * mHeight);
+        aspectRatio = (float) width / (float) height;
+        double scaleDownFactor = 160.0 / Math.max(160.0, dpi);
+        int integrationWidth  = (int) (scaleDownFactor * width);
+        int integrationHeight = (int) (scaleDownFactor * height);
         integrator.resize(integrationWidth, integrationHeight);
-        screenDrawer.resize(integrationWidth, integrationHeight, mWidth, mHeight);
+        screenDrawer.resize(integrationWidth, integrationHeight, width, height);
     }
-
-    private long then = 0;
-    private double recentPerformance = 0.0;
 
     // Rendering thread
     @Override
     public void onDrawFrame(GL10 unused) {
-        RenderState.FrozenState frozenState = renderState.freeze(mAspectRatio);
+        RenderState.FrozenState frozenState = renderState.freeze(aspectRatio);
 
         Texture integratorOutput = integrator.render(frozenState);
         screenDrawer.render(integratorOutput, frozenState);
-
-        long now = System.currentTimeMillis();
-        int milliseconds = (int) (now - then);
-        then = now;
-
-        int frameGoodness = 20 - milliseconds; // minus infinity to about plus 3 (@ 60 FPS)
-        if (frameGoodness >= 0)
-            recentPerformance += 3.0 * (double) frameGoodness;
-        else
-            recentPerformance -= Math.log((double) -frameGoodness);
-
-        if (recentPerformance < -30.0) {
-            if (performanceScalingFactor > 0.125) {
-                performanceScalingFactor /= Math.pow(2.0, 0.125);
-                if (performanceScalingFactor < 0.125)
-                    performanceScalingFactor = 0.125;
-                resizeIntegration();
-            }
-            recentPerformance = 0.0;
-        }
-
-        if (recentPerformance > 30.0) {
-            if (performanceScalingFactor < 1.0) {
-                performanceScalingFactor *= Math.pow(2.0, 0.125);
-                if (performanceScalingFactor > 1.0)
-                    performanceScalingFactor = 1.0;
-                resizeIntegration();
-            }
-            recentPerformance = 0.0;
-        }
     }
 }
