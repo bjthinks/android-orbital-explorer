559aa1212c25e52805b3a5ae3cbc65381f2ea801
Brian Johnson (NUC) <bjthinks@gmail.com>
2016-03-25 12:10:03 -0700

    Add maxradius library

---
+++ maxradius/src/main/java/com/gputreats/orbitalexplorer/MaximumRadiusGenerator.java
@@ -0,0 +1,17 @@
+package com.gputreats.orbitalexplorer;
+
+public class MaximumRadiusGenerator {
+
+    private void go() {
+    }
+
+    public static void main(String args[]) {
+        MaximumRadiusGenerator m = new MaximumRadiusGenerator();
+        try {
+            m.go();
+        } catch (Exception e) {
+            System.err.print(e.getLocalizedMessage());
+            System.exit(1);
+        }
+    }
+}
