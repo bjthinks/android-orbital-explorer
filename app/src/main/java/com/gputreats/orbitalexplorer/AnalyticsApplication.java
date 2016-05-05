498111d5217d271b95b17a4ddc0d457421bb83d2
Brian Johnson (NUC) <bjthinks@gmail.com>
2016-05-04 22:53:02 -0700

    Add Google Analytics Tracker

--- app/src/main/java/com/gputreats/orbitalexplorer/AnalyticsApplication.java
+++ app/src/main/java/com/gputreats/orbitalexplorer/AnalyticsApplication.java
@@ -2,5 +2,20 @@ package com.gputreats.orbitalexplorer;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
 public class AnalyticsApplication extends Application {
+
+    private Tracker tracker;
+
+    synchronized public Tracker getDefaultTracker() {
+        if (tracker == null) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+            tracker = analytics.newTracker(R.xml.global_tracker);
+        }
+        return tracker;
+    }
+
 }
