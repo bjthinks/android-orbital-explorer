cbe13bd21ae46629f5197c02d55c520f876f107b
Brian Johnson (NUC) <bjthinks@gmail.com>
2016-05-11 15:50:33 -0700

    Add ErrorActivity class

---
+++ app/src/main/java/com/gputreats/orbitalexplorer/ErrorActivity.java
@@ -0,0 +1,14 @@
+package com.gputreats.orbitalexplorer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ErrorActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
+
+        setContentView(R.layout.activity_error);
+    }
+}
