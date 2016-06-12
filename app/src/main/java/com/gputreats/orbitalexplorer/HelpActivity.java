6c62bd414d145c3417bf850fdf5c4b913e7a12c8
Brian Johnson (NUC) <bjthinks@gmail.com>
2016-06-11 23:11:34 -0700

    Fix help lifecycle bug

--- app/src/main/java/com/gputreats/orbitalexplorer/HelpActivity.java
+++ app/src/main/java/com/gputreats/orbitalexplorer/HelpActivity.java
@@ -47,7 +47,7 @@ public class HelpActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
+                finish();
                 return true;
         }
         return super.onOptionsItemSelected(item);
