0f60119aed4e0fb5b8981f837d76472c380657c7
Brian Johnson (NUC) <bjthinks@gmail.com>
2017-06-11 15:31:00 -0700

    Add serialVersionUID to OpenGLException

--- app/src/main/java/com/gputreats/orbitalexplorer/OpenGLException.java
+++ app/src/main/java/com/gputreats/orbitalexplorer/OpenGLException.java
@@ -2,6 +2,7 @@ package com.gputreats.orbitalexplorer;
 
 class OpenGLException extends RuntimeException {
 
+    private static final long serialVersionUID = 0;
     private static final int RADIX = 16;
     OpenGLException(int code) {
         super(Integer.toString(code, RADIX));
