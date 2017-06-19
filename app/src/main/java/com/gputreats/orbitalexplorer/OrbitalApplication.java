37a9ef08267eb3aaf57f5d46d44882bedbad02b2
Brian Johnson (NUC) <bjthinks@gmail.com>
2017-06-18 22:20:48 -0700

    Remove Google Analytics completely

--- app/src/main/java/com/gputreats/orbitalexplorer/OrbitalApplication.java
+++ app/src/main/java/com/gputreats/orbitalexplorer/OrbitalApplication.java
@@ -2,22 +2,10 @@ package com.gputreats.orbitalexplorer;
 
 import android.app.Application;
 
-import com.google.android.gms.analytics.GoogleAnalytics;
-import com.google.android.gms.analytics.Tracker;
-
 public class OrbitalApplication extends Application {
-
-    static Tracker tracker;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        if (tracker == null) {
-            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-            if (BuildConfig.DEBUG)
-                analytics.setDryRun(true);
-            tracker = analytics.newTracker(R.xml.global_tracker);
-            tracker.enableAdvertisingIdCollection(true);
-        }
     }
 }
