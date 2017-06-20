58c145a5b3d0c7959fa5e145c9b3a189bb3e7ba5
Brian Johnson (NUC) <bjthinks@gmail.com>
2017-06-19 23:56:07 -0700

    Remove Parcelable interface from RenderState

--- app/src/main/java/com/gputreats/orbitalexplorer/RenderState.java
+++ app/src/main/java/com/gputreats/orbitalexplorer/RenderState.java
@@ -1,10 +1,6 @@
 package com.gputreats.orbitalexplorer;
 
-import android.opengl.GLSurfaceView;
-import android.os.Parcel;
-import android.os.Parcelable;
-
-class RenderState implements Parcelable {
+class RenderState {
 
     private OrbitalView orbitalView;
 
@@ -15,36 +11,9 @@ class RenderState implements Parcelable {
     }
 
     // This happens BEFORE the render thread starts up
-
     synchronized void setOrbitalView(OrbitalView ov) {
         orbitalView = ov;
     }
-
-    // Parcelable stuff
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public synchronized void writeToParcel(Parcel dest, int flags) {
-        dest.writeParcelable(orbital, flags);
-    }
-
-    public static final Parcelable.Creator<RenderState> CREATOR
-            = new Parcelable.Creator<RenderState>() {
-        @Override
-        public RenderState createFromParcel(Parcel source) {
-            RenderState result = new RenderState();
-            result.orbital = source.readParcelable(Orbital.class.getClassLoader());
-            return result;
-        }
-        @Override
-        public RenderState[] newArray(int size) {
-            return new RenderState[size];
-        }
-    };
 
     synchronized Orbital getOrbital() {
         return orbital;
