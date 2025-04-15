fc325f1aab6e7c82941b7ebaff7f5b5d18f3f843
Brian Johnson <bjthinks@gmail.com>
2025-04-15 10:28:58 -0700

    Remove old "enableColor" from AppPreferences

--- app/src/main/java/com/gputreats/orbitalexplorer/AppPreferences.java
+++ app/src/main/java/com/gputreats/orbitalexplorer/AppPreferences.java
@@ -7,18 +7,12 @@ import android.preference.PreferenceManager;
 public class AppPreferences
         implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-    boolean enableColor;
     int colorBlind;
 
     public AppPreferences(Context context) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        enableColor = preferences.getBoolean("prefEnableColor", true);
         colorBlind = Integer.parseInt(preferences.getString("prefColorBlind", "0"));
         preferences.registerOnSharedPreferenceChangeListener(this);
-    }
-
-    public synchronized boolean getEnableColor() {
-        return enableColor;
     }
 
     public synchronized int getColorBlind() {
@@ -27,9 +21,7 @@ public class AppPreferences
 
     @Override
     public synchronized void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
-        if (key.equals("prefEnableColor"))
-            enableColor = preferences.getBoolean("prefEnableColor", true);
-        else if (key.equals("prefColorBlind"))
+        if (key.equals("prefColorBlind"))
             colorBlind = Integer.parseInt(preferences.getString("prefColorBlind", "0"));
     }
 }
