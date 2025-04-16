63b1f5fbf2ef5cb903068edb3d1da3a9a1a4da66
Brian Johnson <bjthinks@gmail.com>
2025-04-16 06:48:43 -0700

    Add an ultra quality checkbox in preferences -- does nothing yet

--- app/src/main/java/com/gputreats/orbitalexplorer/AppPreferences.java
+++ app/src/main/java/com/gputreats/orbitalexplorer/AppPreferences.java
@@ -7,12 +7,18 @@ import android.preference.PreferenceManager;
 public class AppPreferences
         implements SharedPreferences.OnSharedPreferenceChangeListener {
 
+    boolean ultraQuality;
     int colorBlind;
 
     public AppPreferences(Context context) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        ultraQuality = preferences.getBoolean("prefUltraQuality", false);
         colorBlind = Integer.parseInt(preferences.getString("prefColorBlind", "0"));
         preferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    public synchronized boolean getUltraQuality() {
+        return ultraQuality;
     }
 
     public synchronized int getColorBlind() {
@@ -21,7 +27,9 @@ public class AppPreferences
 
     @Override
     public synchronized void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
-        if (key.equals("prefColorBlind"))
+        if (key.equals("prefUltraQuality"))
+            ultraQuality = preferences.getBoolean("prefUltraQuality", false);
+        else if (key.equals("prefColorBlind"))
             colorBlind = Integer.parseInt(preferences.getString("prefColorBlind", "0"));
     }
 }
