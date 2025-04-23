937eaf31013b37d4f22685d9790cf12177af003d
Brian Johnson <bjthinks@gmail.com>
2025-04-22 17:22:54 -0700

    Use mipmaps for arrows

--- app/src/main/java/com/gputreats/orbitalexplorer/AxesDrawer.java
+++ app/src/main/java/com/gputreats/orbitalexplorer/AxesDrawer.java
@@ -69,13 +69,14 @@ public class AxesDrawer {
                 0, GLES30.GL_RED, GLES30.GL_UNSIGNED_BYTE, arrowBuffer);
         // R8 textures are linearly filterable
         GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D,
-                GLES30.GL_TEXTURE_MIN_FILTER, GLES30.GL_LINEAR);
+                GLES30.GL_TEXTURE_MIN_FILTER, GLES30.GL_LINEAR_MIPMAP_LINEAR);
         GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D,
                 GLES30.GL_TEXTURE_MAG_FILTER, GLES30.GL_LINEAR);
         GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D,
                 GLES30.GL_TEXTURE_WRAP_S, GLES30.GL_CLAMP_TO_EDGE);
         GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D,
                 GLES30.GL_TEXTURE_WRAP_T, GLES30.GL_CLAMP_TO_EDGE);
+        GLES30.glGenerateMipmap(GLES30.GL_TEXTURE_2D);
         MyGL.checkGLES();
     }
 
